b96189433ecef2cae36f5b5e323fcb412798a5e8
Kristina Lubinets <kris.lubinec@gmail.com>
2022-08-25 17:29:46 -0400

    Prefix question on leet

---
+++ 14.js
@@ -0,0 +1,20 @@
+//14. Longest Common Prefix
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+
+    if(strs.length === 0) return "";
+    
+    let prefix = "";
+    for(let i = 0; i < strs[0].length; i++) {
+        for(let j = 1; j < strs.length; j++) {
+            if(strs[0][i] !== strs[j][i]) return prefix;
+        }
+        prefix += strs[0][i]
+    }
+    
+    return prefix;
+};
