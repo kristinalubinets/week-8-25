9bdc01f71ed381532e8b3ca7ff55b65aefa48938
Kristina Lubinets <kris.lubinec@gmail.com>
2022-08-29 14:33:59 -0400

    Question on Palindrome string using RegEx

---
+++ 125.js
@@ -0,0 +1,26 @@
+//125. Valid Palindrome using rexEx with replace
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  
+    let changedS = s.toLowerCase().replace(/[^\p{L}\p{N}]/gu, '');    //everything that is -not letters and numbers
+    let reversedS = changedS.split('').reverse().join('');
+       
+    return changedS === reversedS;
+};
+
+// or typing out all of the symbols explicitly:
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+    let changedS = s.toLowerCase().replace(/[."',\/#!?$%\^&\*<>;\s:@{}\[\]=\-_\|/`~()]/g, ''); 
+    let reversedS = changedS.split('').reverse().join('');
+    
+    return changedS === reversedS;
+};
